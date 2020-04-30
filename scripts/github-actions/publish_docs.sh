f07bcf8d593d125ae87798397be39ac6ddd771f7
Patrick Avery <patrick.avery@kitware.com>
2020-04-30 14:27:36 -0400

    fix(ci): do not run doc:publish, as it isn't a command

--- scripts/github-actions/publish_docs.sh
+++ scripts/github-actions/publish_docs.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -ev
 
-git config --global user.name "hydroframe-bot"
-git config --global user.email "hydroframe.bot@gmail.com"
 export GIT_PUBLISH_URL=https://${GITHUB_TOKEN}@github.com/hydroframe/SandTank.git
 npm run semantic-release
-npm run doc:publish
